f4c76d1d1f34fbee898e08156b709a6a4a2527ec
smegulater <georgeharsent@gmail.com>
2018-12-29 23:15:49 +0000

    Camera Movement

---
+++ Unity/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityRTSCore;
+
+public class CameraMovement : MonoBehaviour
+{
+    
+
+    public double ScrollSpeed = 5;
+    public double ScreenEdgeBuffer = 10;
+
+    public ScreenEdgeDirection MousePos;
+    
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        // Get mouse position 
+        CheckMousePos();
+
+        // Check if mouse is near screen boundry
+        // if it is move camera in the direction of the mouse in world space
+
+    }
+
+    private void CheckMousePos()
+    {
+        Vector2 mousePos = Input.mousePosition;
+        mousePos.x -= Screen.width / 2;
+        mousePos.y -= Screen.height / 2;
+
+
+
+        
+    }
+}
---
+++ Unity/Assets/Scripts/Core.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+
+namespace UnityRTSCore
+{
+    public enum ScreenEdgeDirection
+    {
+        Up,
+        Down,
+        Left,
+        Right,
+        LeftUp,
+        RightUp,
+        RightDown,
+        LeftDown,
+        Neutral
+    }
+
+    public static class RTSCore
+    {
+
+
+
+
+    }
+}
+
